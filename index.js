132f4754c09de505ab5bdfcaa65460e8f2d596a9
AduSaater <66807679+AduSaater@users.noreply.github.com>
2023-08-22 13:18:17 +0100

    add customer

--- index.js
+++ index.js
@@ -1,65 +1,15 @@
-const Joi = require('joi');
+const mongoose = require('mongoose');
+const genres = require('./routes/genres');
+const customers = require('./routes/customer');
 const express = require('express');
 const app = express();
 
+mongoose.connect('mongodb://localhost:27017/customers')
+  .then(() => console.log('Connected to MongoDB...'))
+  .catch(err => console.error('Could not connect to MongoDB...'));
+
 app.use(express.json());
-
-const courses = [
-  { id: 1, name: 'Course 1' },  
-  { id: 2, name: 'Course 2' },  
-  { id: 3, name: 'Course 3' },  
-];
-
-app.get('/api/courses', (req, res) => {
-  res.send(courses);
-});
-
-app.post('/api/courses', (req, res) => {
-  const { error } = validateGenre(req.body); 
-  if (error) return res.status(400).send(error.details[0].message);
-
-  const course = {
-    id: courses.length + 1,
-    name: req.body.name
-  };
-  courses.push(course);
-  res.send(course);
-});
-
-app.put('/api/courses/:id', (req, res) => {
-  const course = courses.find(c => c.id === parseInt(req.params.id));
-  if (!course) return res.status(404).send('The course with the given ID was not found.');
-
-  const { error } = validateGenre(req.body); 
-  if (error) return res.status(400).send(error.details[0].message);
-  
- course.name = req.body.name; 
-  res.send(course);
-});
-
-app.delete('/api/courses/:id', (req, res) => {
-  const course = courses.find(c => c.id === parseInt(req.params.id));
-  if (!course) return res.status(404).send('The course with the given ID was not found.');
-
-  const index = courses.indexOf(course);
-  courses.splice(index, 1);
-
-  res.send(course);
-});
-
-app.get('/api/courses/:id', (req, res) => {
-  const course = courses.find(c => c.id === parseInt(req.params.id));
-  if (!course) return res.status(404).send('The course with the given ID was not found.');
-  res.send(course);
-});
-
-function validateGenre(course) {
-  const schema = {
-    name: Joi.string().min(3).required()
-  };
-
-  return Joi.validate(course, schema);
-}
-
+app.use('/api/genres', genres);
+app.use('/api/customers', customers);
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
